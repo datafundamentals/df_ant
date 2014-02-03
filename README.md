a4a4e0ddcabe5ed2c2d0522bd0948b825a32c5ff
jcarapet <jeffcarapetyan@gmail.com>
2014-02-03 16:36:09 -0600

    layout for installing and putting in the packages

---
+++ README.md
@@ -0,0 +1,15 @@
+# df_ant cookbook
+this is used to setup and install the cookbook to properly work with everybody
+# Requirements
+it must be able to work on a CentOS server 6.4 or higher. compatibility for secondary functions is an afterthought.
+# Usage
+
+# Attributes
+
+# Recipes
+this will require a base run of Java before you try to do anything else.
+
+
+# Author
+
+Author:: Jeff Carapetyan (<YOUR_EMAIL>)
---
+++ recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: df_ant
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
+
+# standard setup of the and cookbook is as follows
+# make sure that java is downloaded and installed before an ant install
+# this might need the openjdk over oracle for ease of use
+
+
+# download an ant binary
+
+# unzip said binary
+
+# make sure your JAVA_HOME and ANT_HOME is properly set and that ANT_HOME is in your path
+
+# get the library dependencies for ant
