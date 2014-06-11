f0e8c63ac6073f10546a8a3aa2318727ba89eb29
jcarapet <jeffcarapetyan@gmail.com>
2014-06-11 11:32:52 -0500

    trying to include dependencies

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,5 @@ description      'Installs/Configures df_ant'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.4'
 supports "centos"
+
+depends "df_java"
